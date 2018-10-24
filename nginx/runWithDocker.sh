9af4ddbc7cb67211868f77bdda87b1c97dc5bafa
root <root@localhost.localdomain>
2018-10-24 17:16:16 -0500

    configuraciones

--- nginx/runWithDocker.sh
+++ nginx/runWithDocker.sh
@@ -3,3 +3,6 @@
 docker stop nginx
 docker rm nginx
 docker run -d --name nginx -p 80:80 -v volnginx:/etc/nginx/conf.d nginx
+docker cp default.conf nginx:/etc/nginx/conf.d/
+docker cp nginx.conf nginx:/etc/nginx/
+docker restart nginx
