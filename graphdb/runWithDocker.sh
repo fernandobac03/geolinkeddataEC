eb274a29ca1c8ce93179eec5818c1f8e5c4fa13c
root <root@localhost.localdomain>
2018-10-30 12:06:50 -0500

    configuracion nginx

--- graphdb/runWithDocker.sh
+++ graphdb/runWithDocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop graphdb
-docker rm graphdb
-docker run -d --name graphdb -p 8080:7200  -v graphdbvol:/graphdb/data graphdb-free
-docker cp graphdb.properties graphdb:/graphdb/conf
-docker restart  graphdb
+#docker stop graphdb
+#docker rm graphdb
+#docker run -d --name graphdb -p 7200:7200  -v graphdbvol:/graphdb/data graphdb-free
+docker cp graphdb.properties nginx_graphdb_1:/graphdb/conf
+docker restart  nginx_graphdb_1
---
+++ nginx/configurar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+docker-compose exec site jekyll build
+docker cp default.conf nginx_nginx_1:/etc/nginx/conf.d/
+docker cp nginx.conf nginx_nginx_1:/etc/nginx/
+docker restart nginx_nginx_1
+docker cp ../graphdb/graphdb.properties nginx_graphdb_1:/graphdb/conf
+docker restart nginx_graphdb_1
